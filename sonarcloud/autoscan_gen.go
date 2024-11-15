4dfa6e8036db91b1638f322f1b44b1561cdddb6f
kauppine <24810630+kauppine@users.noreply.github.com>
2024-11-15 19:44:12 +0200

    Add autoscan generated files

---
+++ sonarcloud/autoscan_gen.go
@@ -0,0 +1,101 @@
+package sonarcloud
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/ArgonGlow/go-sonarcloud/sonarcloud/autoscan"
+	"github.com/go-playground/form/v4"
+	"strings"
+)
+
+// AUTOMATICALLY GENERATED, DO NOT EDIT BY HAND!
+
+type Autoscan service
+
+func (s *Autoscan) Eligibility(r autoscan.EligibilityRequest) (*autoscan.EligibilityResponse, error) {
+	params := paramsFrom(r)
+
+	req, err := s.client.GetRequest(fmt.Sprintf("%s/autoscan/eligibility", API), params...)
+	if err != nil {
+		return nil, fmt.Errorf("could not create request: %+v", err)
+	}
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error trying to execute request: %+v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		if errorResponse, err := ErrorResponseFrom(resp); err != nil {
+			return nil, fmt.Errorf("received non 2xx status code (%d), but could not decode error response: %+v", resp.StatusCode, err)
+		} else {
+			return nil, errorResponse
+		}
+	}
+
+	response := &autoscan.EligibilityResponse{}
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, fmt.Errorf("could not decode response: %+v", err)
+	}
+	return response, nil
+}
+
+func (s *Autoscan) Activation(r autoscan.ActivationRequest) error {
+	encoder := form.NewEncoder()
+	values, err := encoder.Encode(r)
+	if err != nil {
+		return fmt.Errorf("could not encode form values: %+v", err)
+	}
+
+	req, err := s.client.PostRequest(fmt.Sprintf("%s/autoscan/activation", API), strings.NewReader(values.Encode()))
+	if err != nil {
+		return fmt.Errorf("could not create request: %+v", err)
+	}
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error trying to execute request: %+v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		if errorResponse, err := ErrorResponseFrom(resp); err != nil {
+			return fmt.Errorf("received non 2xx status code (%d), but could not decode error response: %+v", resp.StatusCode, err)
+		} else {
+			return errorResponse
+		}
+	}
+
+	return nil
+}
+
+func (s *Autoscan) EligibilityStatus(r autoscan.EligibilityStatusRequest) error {
+	encoder := form.NewEncoder()
+	values, err := encoder.Encode(r)
+	if err != nil {
+		return fmt.Errorf("could not encode form values: %+v", err)
+	}
+
+	req, err := s.client.PostRequest(fmt.Sprintf("%s/autoscan/eligibility_status", API), strings.NewReader(values.Encode()))
+	if err != nil {
+		return fmt.Errorf("could not create request: %+v", err)
+	}
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error trying to execute request: %+v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		if errorResponse, err := ErrorResponseFrom(resp); err != nil {
+			return fmt.Errorf("received non 2xx status code (%d), but could not decode error response: %+v", resp.StatusCode, err)
+		} else {
+			return errorResponse
+		}
+	}
+
+	return nil
+}
